cb20104ee143a9d291ecee972d82de0ad7296a87
Adil Aslam <aaliyanaslam22@gmail.com>
2025-04-21 22:51:17 +0500

    half setup  done

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa5iuhhdr7DxBOjmJ-6P23z4rVccaJ1lU",
+  authDomain: "chat-app-4f3d1.firebaseapp.com",
+  projectId: "chat-app-4f3d1",
+  storageBucket: "chat-app-4f3d1.firebasestorage.app",
+  messagingSenderId: "183951095100",
+  appId: "1:183951095100:web:bcad9107a2fba51b595a39"
+};
+
+const app = initializeApp(firebaseConfig);
