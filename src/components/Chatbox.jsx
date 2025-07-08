4a89bce9b09038eab673b80667d9e95fb3c2ad4d
Aaliyan <aaliyanaslam22@gmail.com>
2025-07-08 23:08:38 +0500

    ui set

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -69,7 +69,7 @@ const Chatbox = ({ selectedUser, currentUser }) => {
   }
 
   return (
-    <div className="flex flex-col h-full bg-white">
+    <div className="flex flex-col h-screen bg-white">
       {/* Header */}
       <div className="px-4 py-3 border-b text-lg font-semibold text-gray-700">
         Chat with <span className="text-blue-500">{selectedUser.name || selectedUser.email}</span>
